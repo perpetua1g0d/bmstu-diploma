3c03b9945365bd180616ddf849a9d1f52b50b125
perpetualgod <perpetualgod@yandex.ru>
2025-05-24 13:39:14 +0300

    [iter2]: add postgres-b service, debuging queries...

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -11,27 +11,33 @@ k3d cluster create bmstucluster \
   --k3s-arg "--kubelet-arg=fail-swap-on=false@server:*" \
   --kubeconfig-update-default
 
-docker build -t ghcr.io/perpetua1g0d/bmstu-diploma/sidecar:latest ./sidecar
+# run sidecar code in sidecar containter:
+docker build -t ghcr.io/perpetua1g0d/bmstu-diploma/postgres-sidecar:latest ./postgres-sidecar # todo: postgresql-sidecar
 k3d image import ghcr.io/perpetua1g0d/bmstu-diploma/sidecar:latest -c bmstucluster --keep-tools
 
-kubectl apply -f k8s/00-namespaces/
+kubectl apply -f k8s/namespaces/
+# kubectl apply -k k8s/namespaces/
 
-if ! kubectl get secret ghcr-secret -n postgresql >/dev/null 2>&1; then
-  echo "Creating ghcr.io secret..."
-  kubectl create secret docker-registry ghcr-secret \
-    --docker-server=ghcr.io \
-    --docker-username=perpetua1g0d \
-    --docker-password="$GH_PAT" \
-    --namespace=postgresql
-else
-  echo "ghcr secret already exists, skipped creating secret."
-fi
+namespaces=("postgres-a" "postgres-b" "talos")
+for ns in "${namespaces[@]}"; do
+  if ! kubectl get secret ghcr-secret -n "$ns" >/dev/null 2>&1; then
+    kubectl create secret docker-registry ghcr-secret \
+      --docker-server=ghcr.io \
+      --docker-username=perpetua1g0d \
+      --docker-password="$GH_PAT" \
+      --namespace="$ns"
+    echo "Secret GHCR created in namespace: $ns"
+  else
+    echo "Secret already exists in namespace: $ns"
+  fi
+done
 
 kubectl apply -f k8s/talos/
-kubectl apply -f k8s/postgresql/
+kubectl apply -f k8s/postgresql/postgres-a/
+kubectl apply -f k8s/postgresql/postgres-b/
 # kubectl apply -f k8s/kafka/
 # kubectl apply -f k8s/redis/
-kubectl apply -f k8s/admin-panel/
+# kubectl apply -f k8s/admin-panel/
 
 echo "- Админ-панель: kubectl port-forward -n admin-panel svc/admin-panel 8080:80"
 echo "- PostgreSQL: kubectl port-forward -n postgresql svc/postgresql 5434:5434"
